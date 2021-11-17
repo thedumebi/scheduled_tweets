360d245fb6a3662eecca6b6ef86a6e2d9fc8fa91
thedumebi <chiwuzohdumebi@gmail.com>
2021-11-18 00:58:42 +0100

    user login and logout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,6 @@ class MainController < ApplicationController
   def index
     # flash.now[:notice] = 'Logged in successfully'
     # flash.now[:alert] = 'Invalid email or password'
+    @user = User.find(session[:user_id]) if session[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # GET /about
   get 'about-us', to: 'about#index', as: :about
 
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root to: 'main#index'
 end
