842ee4a2256be1ba43a4de53b2423e323ba6a721
thedumebi <chiwuzohdumebi@gmail.com>
2021-11-18 11:16:59 +0100

    edit password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,5 @@ class MainController < ApplicationController
   def index
     # flash.now[:notice] = 'Logged in successfully'
     # flash.now[:alert] = 'Invalid email or password'
-    @user = User.find(session[:user_id]) if session[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   # GET /about
   get 'about-us', to: 'about#index', as: :about
 
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
   delete 'logout', to: 'sessions#destroy'
 
   root to: 'main#index'
